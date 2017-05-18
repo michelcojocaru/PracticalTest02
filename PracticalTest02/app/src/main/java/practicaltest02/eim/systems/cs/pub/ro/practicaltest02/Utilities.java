4e90609b339c0d014e16fb4f30da84bbe38c125d
Michel-Daniel COJOCARU (25559) <michel.cojocaru@stud.acs.upb.ro>
2017-05-18 14:00:34 +0300

    Solved all tasks.

---
+++ PracticalTest02/app/src/main/java/practicaltest02/eim/systems/cs/pub/ro/practicaltest02/Utilities.java
@@ -0,0 +1,22 @@
+package practicaltest02.eim.systems.cs.pub.ro.practicaltest02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by michelcojocaru on 5/18/17.
+ */
+
+public class Utilities {
+
+    public static BufferedReader getReader(Socket socket) throws IOException {
+        return new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public static PrintWriter getWriter(Socket socket) throws IOException {
+        return new PrintWriter(socket.getOutputStream(), true);
+    }
+}
